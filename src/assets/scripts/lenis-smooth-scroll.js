a273b5186ce59a2403163853de9eba44fbda8b44
Terry <master_correo144@hotmail.com>
2024-08-12 02:26:37 -0500

    SET range and MANY drum hings

--- src/assets/scripts/lenis-smooth-scroll.js
+++ src/assets/scripts/lenis-smooth-scroll.js
@@ -14,10 +14,19 @@ requestAnimationFrame(raf);
 
 document.addEventListener("DOMContentLoaded", () => {
   const containerHero = document.querySelector(".containerHero");
-  lenis.on("scroll", (e) => {
+  lenis.on("scroll", () => {
     if (scrollY > innerHeight) return;
-    containerHero.style.top = scrollY + "px";
+    if (innerWidth > 640) {
+      containerHero.style.top = scrollY + "px";
+    } else {
+      containerHero.style.top = scrollY / 2 + "px";
+    }
     const result = (innerHeight * Math.pow(0.59, scrollY / 100)) / 10;
     containerHero.style.filter = `blur(${result - 2}px)`;
+    if (scrollY > innerHeight - 320) {
+      containerHero.classList.add("larger");
+    } else {
+      containerHero.classList.remove("larger");
+    }
   });
 });
